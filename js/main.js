f1fb4421bc29da6188e7a20a9084f86c35fe6963
juanfezapata <juanfezapata10@gmail.com>
2025-06-03 16:13:59 -0400

    Fix typo in magnifier duration comment

--- js/main.js
+++ js/main.js
@@ -279,7 +279,7 @@
     },
     zoom: {
       enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
+      duration: 300 // don't forget to change the duration also in CSS
     }
   });
 
